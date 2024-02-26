f85e911b17daa319f87f4d31e6f88e4c973c3ce7
Alif T <alift929@gmail.com>
2024-02-26 21:28:28 +0700

    feat: CRUD prompt post

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,62 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt"; 
+
+// GET
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+    const prompt = await Prompt.findById(params.id).populate('creator');
+
+    if(!prompt) return new Response("Prompt not found", { status: 404 });
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (err) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+}
+
+// PATCH
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if(!existingPrompt) return new Response("Prompt not found", { status: 404 });
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (err) {
+    return new Response("Failed to update prompt", { status: 500 });
+  }
+
+}
+
+// DELETE
+export const DELETE = async (request, { params }) => {
+  try {
+      console.log('Deleting prompt with ID:', params.id);
+
+      await connectToDB();
+
+      // Find the prompt by ID and remove it
+      const deletedPrompt = await Prompt.findByIdAndDelete(params.id);
+
+      if (!deletedPrompt) {
+          console.log('Prompt not found');
+          return new Response("Prompt not found", { status: 404 });
+      }
+
+      console.log('Prompt deleted successfully:', deletedPrompt);
+      return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+      console.error('Error deleting prompt:', error);
+      return new Response("Error deleting prompt", { status: 500 });
+  }
+};
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,20 +1,23 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { usePathname, useRouter } from 'next/navigation'
-import Image from 'next/image';
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-  const [copied, setCopied] = useState('');
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+  const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => {
-      setCopied('');
+      setCopied("");
     }, 3000);
-  }
+  };
 
   return (
     <div className="prompt-card">
@@ -22,33 +25,59 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
             src={post.creator.image}
-            alt='user-image'
+            alt="user-image"
             width={40}
             height={40}
-            className='rounded-full object-contain' />
-            <div className="flex flex-col">
-              <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
-              <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
-            </div>
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
         </div>
 
         <div className="copy-btn" onClick={handleCopy}>
-          <Image 
-            src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'}
+          <Image
+            src={
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
             width={12}
             height={12}
           />
         </div>
       </div>
-      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
-        className='font-inter text-sm bg-blue-gradient cursor-pointer'
+        className="font-inter text-sm bg-blue-gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
         {post.tag}
       </p>
-    </div>
-  )
-}
 
-export default PromptCard
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm bg-green-gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm bg-orange-gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PromptCard;
